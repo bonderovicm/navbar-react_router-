0d17b7a60c7616ee4cf8739420cc523205216dd3
Martin <bonderovicmartin8@gmail.com>
2022-10-25 20:10:56 +0200

    typewritter effect for about.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,35 @@
+import Typewriter from "typewriter-effect";
+
 export default function About() {
   return (
     <>
-      <h1>About</h1>
+      <div className="body-container">
+        <div className="text-container">
+          <Typewriter
+            onInit={(typewriter) =>
+              typewriter
+                .typeString("Hello,")
+                .pauseFor(500)
+                .typeString(" my name is Martin")
+                .pauseFor(1000)
+                .pauseFor(500)
+                .typeString(",and I am a web developer.")
+                .pauseFor(600)
+                .typeString(
+                  "I love creating interactive websites that are pleasing to the eye."
+                )
+                .pauseFor(500)
+                .typeString(
+                  " As you can see, I am also very passionate about chess."
+                )
+                .start()
+            }
+          />
+        </div>
+        <div className="video-container">
+  
+        </div>
+      </div>
     </>
   );
 }
